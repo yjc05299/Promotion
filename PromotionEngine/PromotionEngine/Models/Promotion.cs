1ddcb6fab7a29980e6fe8de9d41b85b8d213a09a
Jiachen Yin <jiyi@cubes-software.com>
2021-06-09 11:25:17 +0200

    fixed issue with multiple promotion, calling recursive function

--- PromotionEngine/PromotionEngine/Models/Promotion.cs
+++ PromotionEngine/PromotionEngine/Models/Promotion.cs
@@ -20,29 +20,41 @@ namespace PromotionEngine.Models
       // find all products in the promotion order
       foreach (var p in Products)
       {
-        var matchedProduct = products.FirstOrDefault(_ => _.ID == p.ID) ??
-          order.Products.FirstOrDefault(_ => _.ID == p.ID);
-        // if product in the order not in the list exit or amount is less
-        if (matchedProduct == null || matchedProduct.Amount < p.Amount)
+        var matchedProduct = products.FirstOrDefault(_ => _.ID == p.ID) ??  order.Products.FirstOrDefault(_ => _.ID == p.ID);
+        // if product in the order not in the list  or amount is less exit
+        if (matchedProduct == null || ( matchedProduct.Amount < p.Amount) )
           return null;
         // otherwise add it  to the list
-        if (!matchedProducts.Any(x => x.ID == matchedProduct.ID))
+        if (!matchedProducts.Any(_ => _.ID == matchedProduct.ID))
           matchedProducts.Add(new Product() { ID = matchedProduct.ID , Amount = matchedProduct.Amount});
       }
 
       // loop again to do promotion and take out from the total amount
-          foreach (var p in Products)
-          {
-            var matchedProduct = matchedProducts.FirstOrDefault(_ => _.ID == p.ID);
-            if (matchedProduct != null && matchedProduct.Amount >= p.Amount)
-            {
-              matchedProduct.Amount -= p.Amount;
-              order.TotalPrice += TotalPrice;
-            }
-          }
 
+      decimal leftover = 0;
+      calPromotionRecursive(leftover, matchedProducts,order);
 
       return matchedProducts;
+    }
+
+    public void calPromotionRecursive(decimal leftover, IEnumerable<Product> matchedProducts, Order order)
+    {
+
+      foreach (var p in Products)
+      {
+        var matchedProduct = matchedProducts.FirstOrDefault(_ => _.ID == p.ID);
+        if (matchedProduct != null && matchedProduct.Amount >= p.Amount)
+        {
+          matchedProduct.Amount -= p.Amount;
+          order.TotalPrice += TotalPrice;
+          leftover = matchedProduct.Amount;
+        }
+        if(leftover > p.Amount)
+        {
+          calPromotionRecursive(leftover, matchedProducts, order);
+        }
+
+      }
     }
   }
 }
